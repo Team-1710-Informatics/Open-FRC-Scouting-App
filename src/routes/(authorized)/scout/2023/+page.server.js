85b41885562ee983090f1e57ebeb66a678607f32
Team-1710-Informatics <team1710.io@gmail.com>
2023-03-28 15:30:29 -0500

    TBA function oversight

--- src/routes/(authorized)/scout/2023/+page.server.js
+++ src/routes/(authorized)/scout/2023/+page.server.js
@@ -7,9 +7,7 @@ import { redirect } from "@sveltejs/kit";
 export async function load({ locals, url }) {
     if(!locals.user) throw redirect(307, "/login");
 
-    const res = await tba(`events/2023`);
-
-    const events = await res.json();
+    const events = await tba(`events/2023`);
 
     return{
         events,
